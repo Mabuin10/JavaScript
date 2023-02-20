5805b4fd2c060443be6d2d74800fb9d5e5735448
Matias Abuin Capurro <mabuin5@gmail.com>
2023-02-20 19:48:41 -0300

    correccion de codigo

--- Desktop/CoderHouse/JavaScript/js/main.js
+++ Desktop/CoderHouse/JavaScript/js/main.js
@@ -15,10 +15,12 @@ function saludar() {
     let nombre = prompt("Ingrese su Nombre")
     let apellido = prompt("Ingrese su Apellido")
     let resultado = "Hola " + nombre + " " + apellido + ", este es mi simulador interactivo, por consola te voy a mostrar la multiplicacion de el numero que ingreses por los numeros del 0 al 100 inclusive."
-    alert(resultado)    
+    alert(resultado)
 }
-let numero = prompt("ingresar un numero")
-for (let i = 0; i <= 100; i ++){
-    let resultado = numero * i;
-    console.log(numero + " X " + i + " = " + resultado);
-}
+
+if (username == "usuario" && password == "pass") {
+    let numero = prompt("ingresar un numero")
+    for (let i = 0; i <= 100; i++){
+        let resultado = numero * i;
+        console.log(numero + " X " + i + " = " + resultado)
+    }}
