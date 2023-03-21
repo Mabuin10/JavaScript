bc9b846fa4fba79b37f13f5e724a65eb99d6de5a
Matias Abuin Capurro <mabuin5@gmail.com>
2023-03-21 20:09:46 -0300

    vamos que sale

--- Desktop/CoderHouse/JavaScript/js/main.js
+++ Desktop/CoderHouse/JavaScript/js/main.js
@@ -1,7 +1,7 @@
-const listaDeProductos = [
-    { id: 01, nombre: "JABÓN DE AVENA", descripcion: "Cremoso y delicado, indicado para piel sensible, propensa a irritaciones y enrojecimiento, sin aroma. Jabón saponificado en frío.Elaborado con aceites y mantecas vegetales, enriquecido con avena. Sin químicos, ni crueldad animal.", size:"Corte artesanal 110gr. aproximadamente", precio: 490, img: "../assets/img/jabon-avena.jpeg" },
-    { id: 02, nombre: "JABÓN DE CAFÉ DETOX", descripcion: "Exfoliante detox, desintoxica, elimina células muertas e impurezas, reactiva la circulación, previene la aparición de las arañitas y previene celulitis. Enriquecido con café y con avena, Sin químicos, ni crueldad animal.", size:"Corte artesanal 110gr. aproximadamente", precio: 490, img: "../assets/img/jabon-cafe.jpeg" },
-    { id: 03, nombre: "SHAMPOO SÓLIDO NORMAL", descripcion: "Ésta variedad es ideal para todo tipo de pelo, es nutritivo, evita la anticaspa y la caída. Realizado con materia prima 100% natural⁣, con tensioactivo SCI, arcillas hierbas y aceites vegetales. Sin químicos, ni crueldad animal.", size:"80gr rinde 60/70 lavados aproximadamente", precio: 490, img: "../assets/img/galeria5.jpeg" },
+let listaDeProductos = [
+    { id: 01, nombre: "JABÓN DE AVENA", descripcion: "Cremoso y delicado, indicado para piel sensible, propensa a irritaciones y enrojecimiento, sin aroma. Jabón saponificado en frío.Elaborado con aceites y mantecas vegetales, enriquecido con avena. Sin químicos, ni crueldad animal.", size: "Corte artesanal 110gr. aproximadamente", precio: 490, img: "../assets/img/jabon-avena.jpeg", alt:"jabon de avena" },
+    { id: 02, nombre: "JABÓN DE CAFÉ DETOX", descripcion: "Exfoliante detox, desintoxica, elimina células muertas e impurezas, reactiva la circulación, previene la aparición de las arañitas y previene celulitis. Enriquecido con café y con avena, Sin químicos, ni crueldad animal.", size: "Corte artesanal 110gr. aproximadamente", precio: 490, img: "../assets/img/jabon-cafe.jpeg", alt:"jabon de cafe" },
+    { id: 03, nombre: "SHAMPOO SÓLIDO NORMAL", descripcion: "Ésta variedad es ideal para todo tipo de pelo, es nutritivo, evita la anticaspa y la caída. Realizado con materia prima 100% natural⁣, con tensioactivo SCI, arcillas hierbas y aceites vegetales. Sin químicos, ni crueldad animal.", size: "80gr rinde 60/70 lavados aproximadamente", precio: 490, img: "../assets/img/galeria5.jpeg", alt:"shampoo normal" },
     /*{id: 04, nombre: "JABÓN DE AVENA", descripcion: "Cremoso y delicado, indicado para piel sensible, propensa a irritaciones y enrojecimiento, sin aroma. Jabón saponificado en frío.Elaborado con aceites y mantecas vegetales, enriquecido con avena. Sin químicos, ni crueldad animal. Corte artesanal 110gr. aproximadamente", precio: 490}
     {id: 05, nombre: "JABÓN DE AVENA", descripcion: "Cremoso y delicado, indicado para piel sensible, propensa a irritaciones y enrojecimiento, sin aroma. Jabón saponificado en frío.Elaborado con aceites y mantecas vegetales, enriquecido con avena. Sin químicos, ni crueldad animal. Corte artesanal 110gr. aproximadamente", precio: 490}
     {id: 06, nombre: "JABÓN DE AVENA", descripcion: "Cremoso y delicado, indicado para piel sensible, propensa a irritaciones y enrojecimiento, sin aroma. Jabón saponificado en frío.Elaborado con aceites y mantecas vegetales, enriquecido con avena. Sin químicos, ni crueldad animal. Corte artesanal 110gr. aproximadamente", precio: 490}
@@ -11,33 +11,123 @@ const listaDeProductos = [
 */
 ]
 
-const contenedorProductos = document.getElementById("contenedor-productos")
+const listadejson = JSON.stringify(listaDeProductos)
 
-listaDeProductos.forEach( producto => {
-    contenedorProductos.innerHTML += `
-            <div class="row">
-                <div class="col-sm-12 col-md-6 col-lg-4">
-                    <div class="card mb-3 mx-auto" style="max-width: 540px;">
-                        <div class="row g-0">
-                            <div class="col-md-4">
-                                <img src=${producto.img} class="img-fluid rounded-start"
-                                    alt="jabon de avena">
-                            </div>
-                            <div class="col-md-8">
-                                <div class="card-body wow animate__animated animate__bounceIn">
-                                    <h5 class="card-title">${producto.nombre}</h5>
-                                    <p class="card-text">${producto.descripcion}</p>
-                                    <small class="text-muted">${producto.size}</small>
-                                    <small class="fs-4">$${producto.precio}</small>
+localStorage.setItem("listaDeProductos",listadejson)
+
+class ProdController {
+    constructor(){
+        this.listaDeProductos = []
+    }
+
+    levantar(){
+        let traerJson = localStorage.getItem("listaDeProductos")
+
+        if (traerJson){
+            this.listaDeProductos = JSON.parse(traerJson)
+        }
+    }
+
+    mandarAlDom(contenedorProductos){
+        
+        contenedorCarrito.innerHTML = ""
+        
+
+        this.listaDeProductos.forEach(producto => {
+            contenedorProductos.innerHTML += `
+                    <div class="row">
+                        <div class="col-sm-12 col-md-6 col-lg-4">
+                            <div class="card mb-3 mx-auto" style="max-width: 540px;">
+                                <div class="row g-0">
+                                    <div class="col-md-4">
+                                        <img src=${producto.img} class="img-fluid rounded-start"
+                                            alt="jabon de avena">
+                                    </div>
+                                    <div class="col-md-8">
+                                        <div class="card-body wow animate__animated animate__bounceIn">
+                                            <h5 class="card-title">${producto.nombre}</h5>
+                                            <p class="card-text">${producto.descripcion}</p>
+                                            <small class="text-muted">${producto.size}</small>
+                                            <small class="fs-4">$${producto.precio}</small>
+                                        </div>
+                                    </div>
+                                    <div class="d-grid gap-2">
+                                            <button class="btn btn-success" id="btnProd${producto.id}" type="button">Agregar al Carrito</button>
+                                    </div>
                                 </div>
                             </div>
-                            <div class="d-grid gap-2">
-                                    <button class="btn btn-success" type="button">Agregar al Carrito</button>
-                            </div>
-                        </div>
+                        </div>`
+        })
+    }
+}
+
+class CartController {
+    constructor(){
+        this.listaDeCarrito = []
+    }
+    levantar(){
+        let traerJson = localStorage.getItem("listaDeCarrito")
+
+        if (traerJson){
+            this.listaDeCarrito = JSON.parse(traerJson)
+        }
+    }
+
+    cargarCarrito(producto){
+        this.listaDeCarrito.push(producto)
+        let listadejson = JSON.stringify(this.listaDeCarrito)
+        localStorage.setItem("listaDeCarrito", listadejson)
+    }
+
+    mandarAlDom(contenedorCarrito){
+
+        contenedorCarrito.innerHTML = ""
+
+        this.listaDeCarrito.forEach(producto =>{
+
+        contenedorCarrito.innerHTML += `
+        <div class="card mb-3" style="max-width: 540px;">
+            <div class="row g-0">
+                <div class="col-md-4">
+                    <img src="${producto.img}" class="img-fluid rounded-start" alt="...">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h5 class="card-title">${producto.nombre}</h5>
+                        <p class="card-text">${producto.descripcion}</p>
+                        <p class="card-text"><small class="fs-5">Precio :$${producto.precio}</small></p>
                     </div>
-                </div>`
+                </div>
+            </div>
+        </div>`
+        })
+    }
+}
+
+const controladorProductos = new ProdController ()
+const controladorCarrito = new CartController()
+
+controladorProductos.levantar()
+controladorCarrito.levantar()
+
+const contenedorProductos = document.getElementById("contenedor-productos")
+const contenedorCarrito = document.getElementById("contenedor-carrito")
+
+controladorProductos.mandarAlDom(contenedorProductos)
+controladorCarrito.mandarAlDom(contenedorCarrito)
+
+
+controladorProductos.listaDeProductos.forEach( producto => {
+    const subirAlCarrito = document.getElementById(`btnProd${producto.id}`)
+
+    subirAlCarrito.addEventListener("click",()=>{
+        
+        controladorCarrito.cargarCarrito(producto)
+        controladorCarrito.levantar()
+        controladorCarrito.mandarAlDom(contenedor-carrito)
+    })
 })
+
 
 /*class Productos {
     constructor(id, nombre, precio, cantidad) {
